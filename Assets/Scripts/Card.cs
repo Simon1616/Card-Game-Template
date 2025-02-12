94510bf55adcf7cdd191ac05e5b5f886b6cab9c6
Simon1616 <scastle5566@student.pps.net>
2025-02-12 15:30:12 -0800

    card complete

--- Assets/Scripts/Card.cs
+++ Assets/Scripts/Card.cs
@@ -10,11 +10,15 @@ public class Card : MonoBehaviour
 
     public string card_name;
     public string value;
-    public Sprite image; // Changed from image to sprite to match Card_data
+    public Sprite image;
+    public string card_name_copy;
+    public string value_copy;
 
     public TextMeshProUGUI nameText;
     public TextMeshProUGUI valueText;
-    public Image spriteImage; // Corrected type to Image
+    public Image spriteImage;
+    public TextMeshProUGUI nameText_copy;
+    public TextMeshProUGUI valueText_copy;
 
     // Start is called before the first frame update
     void Start()
@@ -22,10 +26,14 @@ public class Card : MonoBehaviour
         card_name = data.card_name;
         value = data.value;
         image = data.image;
+        card_name_copy = data.card_name;
+        value_copy = data.value;
 
         nameText.text = card_name;
         valueText.text = value;
-        spriteImage.sprite = image; // Changed from image to sprite
+        spriteImage.sprite = image;
+        nameText_copy.text = card_name_copy;
+        valueText_copy.text = value_copy;
     }
 
     // Update is called once per frame
